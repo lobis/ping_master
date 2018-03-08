fe37724ed93951a02d5a0bff8e2c7bfeb9b34492
luisobis <luis.antonio.obis@gmail.com>
2018-03-08 15:24:35 +0100

    first commit

---
+++ pingman.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask import render_template
+from flask_sockets import Sockets
+
+import time
+
+from ping import ping
+
+app = Flask(__name__)
+sockets = Sockets(app)
+
+hosts = []
+with open('hosts') as f:
+    for line in f:
+        hosts.append([x for x in line.split()])
+
+@app.route('/')
+def main():
+    #return render_template('pingman.html', ping=ping, hosts=hosts)
+    return render_template("socket.html")
+
+@sockets.route('/echo')
+def echo_socket(ws):
+    while True:
+        ws.send("test")
+        print("sending...")
+        time.sleep(2)
+
+if __name__ == '__main__':
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+    server.serve_forever()
